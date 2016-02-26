9b46954ad420d229f8af2199c96c1c3fb2c33d65
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2016-02-26 23:20:11 +0100

    new GUI

--- Main.py
+++ Main.py
@@ -1,25 +1,43 @@
 import numpy as np
 import cv2
-import Tkinter
+from Tkinter import *
 import tkFileDialog
 import os
 import CellCounter as cc
 
 imageNameList = []
 
-def browseButtonCallBack():
-    dirName = tkFileDialog.askdirectory()
-    (_, _, filenames) = os.walk(dirName).next()
-    for filename in filenames:
-        if (filename.endswith('.tif')):
-            imageNameList.append(filename)
+class App:
 
-    for imageName in imageNameList:
-        cc.countCells(imageName)
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.browseButton = Button(top, text ="Choose directory", command = self.browseButtonCallBack)
+        self.browseButton.pack(side=LEFT)
+
+        self.fileButton = Button(top, text ="Choose file", command = self.fileButtonCallBack)
+        self.fileButton.pack(side=LEFT)
+
+    def browseButtonCallBack(self):
+        dirName = tkFileDialog.askdirectory()
+        (_, _, filenames) = os.walk(dirName).next()
+        for filename in filenames:
+            if (filename.endswith('.tif')):
+                imageNameList.append(dirName + '/' + filename)
+
+        for imageName in imageNameList:
+            numPix, im = cc.countCells(imageName)
+            cv2.namedWindow(str(numPix), cv2.WINDOW_AUTOSIZE);
+            cv2.imshow(str(numPix), im);
+
+    def fileButtonCallBack(self):
+        fileName = tkFileDialog.askopenfilename()
+        numPix, im = cc.countCells(fileName)
+        cv2.namedWindow(str(numPix), cv2.WINDOW_AUTOSIZE);
+        cv2.imshow(str(numPix), im);
 
 
+top = Tk()
+app = App(top)
 
-top = Tkinter.Tk()
-browseButton = Tkinter.Button(top, text ="Choose directory", command = browseButtonCallBack)
-browseButton.pack()
 top.mainloop()
