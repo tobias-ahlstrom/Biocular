33b049882041c84f7383d33c9cc761e8bd901c12
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2016-03-01 22:00:44 +0100

    hysteresis working

--- CellCounter.py
+++ CellCounter.py
@@ -1,59 +1,64 @@
-import numpy as np
+"""Cell counter."""
 import cv2
 import LocalMaxima as lm
+import MyWaterShed as ws
+
 
 def countCells(imagePath):
-
+    """Function for counting cells."""
     # Load and show image
     image = cv2.imread(imagePath, cv2.IMREAD_COLOR)
 
-    # cv2.namedWindow("Original Image", cv2.WINDOW_AUTOSIZE)
-    # cv2.imshow("Original Image", image)
+    cv2.namedWindow("Original Image", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow("Original Image", image)
 
     # Extract red channel and show it
-    imageR = image[:,:,2]
-    # cv2.namedWindow("Red Channel", cv2.WINDOW_AUTOSIZE)
-    # cv2.imshow("Red Channel", imageR)
+    imageR = image[:, :, 2]
+    cv2.namedWindow("Red Channel", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow("Red Channel", imageR)
 
+    ws.mywatershed(image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-    # Filter with negative laplacian filter and show result
-    # imageR_laplacian = cv2.Laplacian(imageR, -1, 3, -1, 0, cv2.BORDER_DEFAULT)
-    imageR_laplacian = None
-    imageR_laplacian = cv2.Laplacian(imageR, -1, imageR_laplacian, 3, -1)
-
+    # # Filter with negative laplacian filter and show result
+    # imageR_laplacian = None
+    # imageR_laplacian = cv2.Laplacian(imageR, -1, imageR_laplacian, 3, -1)
+    #
     # cv2.namedWindow("Laplacian", cv2.WINDOW_AUTOSIZE)
     # cv2.imshow("Laplacian", imageR_laplacian)
-
-    # Threshold the filtered image
-    retval,imageR_laplacian_t = cv2.threshold(imageR_laplacian, 200, 1, cv2.THRESH_BINARY)
-
+    #
+    # # Threshold the filtered image
+    # retval, imageR_laplacian_t = cv2.threshold(imageR_laplacian, 200, 1,
+    #                                            cv2.THRESH_BINARY)
+    #
     # cv2.namedWindow("Thresholded", cv2.WINDOW_AUTOSIZE)
     # cv2.imshow("Thresholded", imageR_laplacian_t*255)
-
-    # Multiply the thresholded image and the filtered one
-    imageR_mult = cv2.multiply(imageR_laplacian, imageR_laplacian_t)
-
+    #
+    # # Multiply the thresholded image and the filtered one
+    # imageR_mult = cv2.multiply(imageR_laplacian, imageR_laplacian_t)
+    #
     # cv2.namedWindow("Multiplied", cv2.WINDOW_AUTOSIZE)
     # cv2.imshow("Multiplied", imageR_mult)
-
-    # Find regional maxima of the multiplied image
-    imageR_regionalmax = lm.localMaxima(imageR_mult, 15)
-
+    #
+    # # Find regional maxima of the multiplied image
+    # imageR_regionalmax = lm.localMaxima(imageR_mult, 15)
+    #
     # cv2.namedWindow("imageR_regionalmax", cv2.WINDOW_AUTOSIZE)
     # cv2.imshow("imageR_regionalmax", imageR_regionalmax)
-
-    # Make circels around the red dots
-    height, width = imageR_regionalmax.shape
-    for i in range(0, height):
-        for j in range(0, width):
-            if (imageR_regionalmax[i,j] != 0):
-                cv2.circle(image, (j,i), 4, (0,255,255))
-
-    # Show the circled image
-    # cv2.namedWindow("Original Image with circles", cv2.WINDOW_AUTOSIZE);
-    # cv2.imshow("Original Image with circles", image);
-
-
-    # Print out the number of pixels
-    numberOfPixels = cv2.countNonZero(imageR_regionalmax)
-    return numberOfPixels, image
+    #
+    # # Make circels around the red dots
+    # height, width = imageR_regionalmax.shape
+    # for i in range(0, height):
+    #     for j in range(0, width):
+    #         if (imageR_regionalmax[i, j] != 0):
+    #             cv2.circle(image, (j, i), 4, (0, 255, 255))
+    #
+    # # Show the circled image
+    # cv2.namedWindow("Original Image with circles", cv2.WINDOW_AUTOSIZE)
+    # cv2.imshow("Original Image with circles", image)
+    #
+    # # Print out the number of pixels
+    # numberOfPixels = cv2.countNonZero(imageR_regionalmax)
+    # return numberOfPixels, image
+    return
--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+"""Main program."""
 import numpy as np
 import cv2
 from Tkinter import *
@@ -7,16 +8,23 @@ import CellCounter as cc
 
 imageNameList = []
 
+
 class App:
+    """The app with GUI."""
 
     def __init__(self, master):
+        """Constructor."""
         frame = Frame(master)
         frame.pack()
-        self.browseButton = Button(top, text ="Choose directory", command = self.browseButtonCallBack)
+        self.browseButton = Button(top, text="Choose directory",
+                                   command=self.browseButtonCallBack)
         self.browseButton.pack(side=LEFT)
 
-        self.fileButton = Button(top, text ="Choose file", command = self.fileButtonCallBack)
+        self.fileButton = Button(top, text="Choose file", command=self.fileButtonCallBack)
         self.fileButton.pack(side=LEFT)
+
+        self.testButton = Button(top, text="TEST", command=self.testButtonCallBack)
+        self.testButton.pack(side=LEFT)
 
     def browseButtonCallBack(self):
         dirName = tkFileDialog.askdirectory()
@@ -27,14 +35,21 @@ class App:
 
         for imageName in imageNameList:
             numPix, im = cc.countCells(imageName)
-            cv2.namedWindow(str(numPix), cv2.WINDOW_AUTOSIZE);
-            cv2.imshow(str(numPix), im);
+            cv2.namedWindow(str(numPix), cv2.WINDOW_AUTOSIZE)
+            cv2.imshow(str(numPix), im)
+        return
 
     def fileButtonCallBack(self):
         fileName = tkFileDialog.askopenfilename()
-        numPix, im = cc.countCells(fileName)
-        cv2.namedWindow(str(numPix), cv2.WINDOW_AUTOSIZE);
-        cv2.imshow(str(numPix), im);
+        # numPix, im = cc.countCells(fileName)
+        cc.countCells(fileName)
+        # cv2.namedWindow(str(numPix), cv2.WINDOW_AUTOSIZE)
+        # cv2.imshow(str(numPix), im)
+        return
+
+    def testButtonCallBack(self):
+        cc.countCells('Parent-CHPT1-fish118.tif')
+        return
 
 
 top = Tk()
